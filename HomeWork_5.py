d2090ae8962942b0a2cb7261f686f3b503057633
AzarovDN <azarovdn@icloud.com>
2018-11-22 09:23:27 +1000

    Исправил ошибки. Убрал не используемый импорт. Использовал методы сбора и кормления на курице. Переделал метод hungry()

--- HomeWork_5.py
+++ HomeWork_5.py
@@ -1,39 +1,44 @@
-import weakref
-
-
 class Animal:
     weight = 0  # kg
-    want_to_eat = 'Yes'
+    want_to_eat = 100  # %
     name = 'Unnamed'
 
     def __init__(self, weight, name):
         self.weight = weight
         self.name = name
 
-    def hungry(self):
-        self.want_to_eat = 'No'
+    def hungry(self, feed):
+        self.want_to_eat -= feed  # Количество корма указываем в %
 
 
 class Birds(Animal):
-    eggs = 1  # ps.
+    eggs = 0
+
+    def give_egg(self):
+        self.eggs += 1
+        self.want_to_eat += 20
 
     def collect_eggs(self):
+        my_eggs += self.eggs
         self.eggs = 0
 
 
 class Animal_give_milk(Animal):
-    have_milk = 100  # %
+    have_milk = 10  # Литры
 
     def milk(self):
         self.have_milk = 0
+        my_milk += have_milk
 
 
 class Sheeps(Animal):
     say = 'Бе-бе'
-    need_to_cut = 100  # %
+    need_to_cut = 10  # см
 
-    def cut(self):
-        self.need_to_cut = 0
+    def cut(self, length):
+        self.need_to_cut = self.need_to_cut - length
+        my_wool += length * .2  # предположим, что каждый сантиметра шерсти, сбритый с поверхности овцы дает 200 грамм
+
 
 
 class Cows(Animal_give_milk):
@@ -75,6 +80,25 @@ duck = Ducks(2, 'Кряква')
 animals = [goose_gray, goose_white, cow, sheep_barashek, sheep_kudryash, chiken_ko, chiken_ku, goat_roga, goat_kopyta,
            duck]
 
+print ('Курица голодна на {} %'.format(chiken_ko.want_to_eat),'\n')
+
+print ('Вызвали метод кормить','\n')
+
+chiken_ko.hungry(30)
+
+print ('Курица голодна на {} %'.format(chiken_ko.want_to_eat))
+
+print ('У курицы {} яиц'.format(chiken_ko.eggs),'\n')
+
+chiken_ko.give_egg()
+
+print ('Вызвали метод, курица снесла яйцо','\n')
+
+print ('Курица голодна на {} %'.format(chiken_ko.want_to_eat))
+
+print ('У курицы {} яиц'.format(chiken_ko.eggs),'\n')
+
+
 all_weight = 0
 max_weight = 0
 for animal in animals:
@@ -84,7 +108,7 @@ for animal in animals:
         heaviest_animal = animal.name
 
 print('Суммарный вес всех животных {} кг'.format(all_weight))
-print('Самое тяжелое животное {} весит {} кг.'.format(heaviest_animal, max_weight))
+print('Самое тяжелое животное {} весит {} кг.'.format(heaviest_animal, max_weight),'\n')
 
 
 # вариант 2 решил через модуль, толком про него не чилал, но все работает.
